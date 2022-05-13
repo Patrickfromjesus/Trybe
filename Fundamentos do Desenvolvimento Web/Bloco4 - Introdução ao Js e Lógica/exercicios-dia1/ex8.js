d66e5704ff25f707dc63215accab697f20e702a8
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-13 09:36:11 -0300

    Adição do exercício 8.

---
+++ Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia1/ex8.js
@@ -0,0 +1,10 @@
+const first = 19;
+const second = 21;
+const third = 7;
+
+if((first % 2 == 0) || (second % 2 == 0) || (third % 2 == 0)) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}

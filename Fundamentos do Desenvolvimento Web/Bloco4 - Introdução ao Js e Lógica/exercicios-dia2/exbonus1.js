0c1d42de9db3e81402a31aceba7c5d07353f1148
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-16 11:51:03 -0300

    Adição de exercícios padrão e bônus 1.

---
+++ Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia2/exbonus1.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i < numbers.length - 1; i++) {
+    for(let p = 0; p < numbers.length - 1; p++) {
+        let box = 0;
+        if(numbers[p] > numbers[p + 1]) {
+            box += numbers[p];
+            numbers[p] = numbers[p + 1];
+            numbers[p + 1] = box;
+        }
+    }
+}
+console.log(numbers);

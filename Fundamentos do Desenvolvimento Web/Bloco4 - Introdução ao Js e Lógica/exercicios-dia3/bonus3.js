74a39ab5db23135a4b4a48f44c86c166a11531c9
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-17 09:24:50 -0300

    Adição do ex bônus 3.

---
+++ Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia3/bonus3.js
@@ -0,0 +1,12 @@
+const side = 5;
+
+for(let p = 1; p <= side; p++) {
+    let chunk = "";
+    for(let i = side; i > p; i--) {
+        chunk += " ";
+    }
+    for(let q = 0; q < p; q++) {
+    chunk += "*";
+    }
+    console.log(chunk);
+}

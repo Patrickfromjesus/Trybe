775d417bb4290e05fe3ef1dd8ed08b8c26acfcf4
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-17 12:39:00 -0300

    Adição ex bônus 5 pronto.

--- Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia3/bonus5.js
+++ Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia3/bonus5.js
@@ -1,20 +1,42 @@
-let width = 4;
+let width = 9;
 let iterator = 0;
+let count = 0;
+let copy = width;
 
-for(let  i = 0; i < width; i++) {
-    let chunk = "";
-    iterator += 2;    
-
-    for(let p = 1; p < width - i; p++) {
-        chunk += " ";
+//descobrir qual será a altura da pirâmide.
+for(let i = 0; i < width; i++) {
+    if(copy >= 0) {
+        count++;
+        copy -= 2;
     }
+}
 
-    for(let p = 1; p < iterator; p++) {
-        chunk += "*";
-    }
+//iterar tantas vezes quanto sua altura, descoberta anteriormente.
+for(let  i = 0; i < count; i++) {
+    let chunk = ""
+    iterator += 2;
 
-    for(let p = 1; p < width - 1; p++) {
-        chunk += " ";
-    }
+        for(let q = 1; q < width / 2 - i; q++) {
+            chunk += " ";    
+        }
+
+        for(let q = 1; q < iterator ; q++) {
+            if(i !== count - 1) { //se não estiver na última linha, tira os asteriscos do meio.
+
+                if(q === 1 || q === iterator - 1) { //se for os asteriscos das pontas, não tirará os asteriscos.
+                    chunk += "*";
+                }
+                else{
+                    chunk += " "; //se não for os das pontas, coloca espaço.
+                }
+            }
+            else {
+                chunk += "*";
+            }    
+        }
+
+        for(let q = 1; q < width / 2 - i; q++) {
+            chunk += " ";    
+        }
     console.log(chunk);
 }

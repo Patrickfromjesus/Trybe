1016b74446ed673e133cac4338fa7b86cb1d8b5c
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-18 11:02:05 -0300

    Adição do ex 5 de object e for.

---
+++ Fundamentos do Desenvolvimento Web/Bloco4 - Introdução ao Js e Lógica/exercicios-dia4/ex5.js
@@ -0,0 +1,23 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+    maisCoisa: {
+        personagem:'Tio Patinhas',
+        origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178', 
+        nota: 'O último MacPatinhas', 
+        recorrente: 'Sim',
+    }
+};
+
+for(let index in info) {
+    
+    if(index === 'recorrente') {
+        console.log("Ambos são recorrentes.");
+    }
+    else if(index !== 'maisCoisa') {
+        console.log(`${info[index]} e ${info.maisCoisa[index]}.`);
+    }
+}
+

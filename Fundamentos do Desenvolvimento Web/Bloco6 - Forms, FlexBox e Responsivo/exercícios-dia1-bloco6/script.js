210eefa72eeeb91fb0572c0bb5e7090b4352133c
Gomes Conceição <patrick_epcar17@outlook.com>
2022-05-31 18:20:15 -0300

    Adição dos exercícios bônus.

--- Fundamentos do Desenvolvimento Web/Bloco6 - Forms, FlexBox e Responsivo/exercícios-dia1-bloco6/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco6 - Forms, FlexBox e Responsivo/exercícios-dia1-bloco6/script.js
@@ -1,4 +1,14 @@
 let getButton = document.querySelector('#submit-button');
+let checkBox = document.querySelector('#would-like2');
+
 getButton.addEventListener('click', function(e) {
+    let getName = document.querySelector('#name').value.length;
+    let getEmail = document.querySelector('#email').value.length;
+    let getTextarea = document.querySelector('#why-me').value.length;
+    if(getName >= 10 && getName <= 40 && getEmail >= 10 && getEmail <= 50 && getTextarea <= 500 && checkBox.checked) {
+        window.alert('Dados enviados com sucesso!');
+        return;
+    }
     e.preventDefault();
+    window.alert('Dados inválidos!');
 })

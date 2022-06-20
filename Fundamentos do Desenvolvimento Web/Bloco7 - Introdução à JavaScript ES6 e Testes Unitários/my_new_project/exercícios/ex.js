87090205b5fdd79423b404cb5831385ce0c41243
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-20 10:54:38 -0300

    Adição do ex 3 pronto.

--- Fundamentos do Desenvolvimento Web/Bloco7 - Introdução à JavaScript ES6 e Testes Unitários/my_new_project/exercícios/ex.js
+++ Fundamentos do Desenvolvimento Web/Bloco7 - Introdução à JavaScript ES6 e Testes Unitários/my_new_project/exercícios/ex.js
@@ -17,4 +17,20 @@ function myRemove(arr, item) {
   return copyArr;
 }
 
-module.exports = {sum, myRemove};
+function myFizzBuzz(num) {
+  if(typeof(num) != 'number') {
+    throw new Error("Not a number.");
+  }
+  else if(num % 15 === 0) {
+    return 'fizzbuzz';
+  }
+  else if(num % 3 === 0 && num % 5 !== 0) {
+    return 'fizz';
+  }
+  else if(num % 5 === 0 && num % 3 !== 0) {
+    return 'buzz';
+  }
+  return num;
+}
+
+module.exports = {sum, myRemove, myFizzBuzz};
--- Fundamentos do Desenvolvimento Web/Bloco7 - Introdução à JavaScript ES6 e Testes Unitários/my_new_project/exercícios/ex.test.js
+++ Fundamentos do Desenvolvimento Web/Bloco7 - Introdução à JavaScript ES6 e Testes Unitários/my_new_project/exercícios/ex.test.js
@@ -1,5 +1,6 @@
-const {sum, myRemove} = require('./ex');
+const {sum, myRemove, myFizzBuzz} = require('./ex');
 
+//Ex 1
 describe('Test if sum function is correct.', () => {
   it('test if sum(4,5) is 9', () => {
     expect(sum(0,0)).toBe(0);
@@ -9,6 +10,7 @@ describe('Test if sum function is correct.', () => {
   })
 })
 
+//Ex 2
 describe('Test if remove correct item', () => {
   const arr = [1, 2, 3, 4];
   it('test if myRemove([1, 2, 3, 4], 3) works well', () => {
@@ -21,5 +23,28 @@ describe('Test if remove correct item', () => {
 
   it('test if myRemove([1, 2, 3, 4], 5) works well', () => {
     expect(myRemove(arr, 5)).toEqual([1,2,3,4]);
+  })
+})
+
+//Ex 3
+describe('Test if FizzBuzz works well', () => {
+  it('Test if returns fizzbuzz', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  })
+
+  it('Test if returns fizz', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+  })
+
+  it('Test if returns buzz', () => {
+    expect(myFizzBuzz(10)).toBe('buzz');
+  })
+
+  it('Test if returns number', () => {
+    expect(myFizzBuzz(11)).toBe(11);
+  })
+
+  it('Test if returns error', () => {
+    expect(() => myFizzBuzz('9')).toThrow(Error);
   })
 })

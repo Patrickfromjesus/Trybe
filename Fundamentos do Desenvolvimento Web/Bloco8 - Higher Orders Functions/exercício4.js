e74925bc4f965eea512de9aa7778876d46b8916f
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-24 10:29:23 -0300

    Adição do exercício 4 prática pronto.

--- Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício4.js
+++ Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício4.js
@@ -1,1 +1,8 @@
 const {books} = require('./exercícios.js');
+
+//Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+const now = new Date().getFullYear();
+const booksOver60 = books.filter((obj) => (now - obj.releaseYear > 60));
+booksOver60.sort((a, b) => a.releaseYear - b.releaseYear);
+
+console.log(booksOver60);

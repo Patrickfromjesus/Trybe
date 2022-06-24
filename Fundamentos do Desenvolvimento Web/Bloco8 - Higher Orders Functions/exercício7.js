bfd7b21874e6151917675dd543a6979add28088c
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-24 11:25:14 -0300

    Adição do exercício 7 prática pronto.

--- Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício7.js
+++ Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício7.js
@@ -1,1 +1,15 @@
 const {books} = require('./exercícios.js');
+
+//Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const book = books.find((obj) => {
+  let count = 0;
+  const name = obj.author.name.substring(0,8);
+  for(let i = 0; i < 8; i++) {
+    if(name[i] === '.') count++;
+  }
+  return count === 3; 
+})
+const nameBook = book.name;
+
+console.log(nameBook);

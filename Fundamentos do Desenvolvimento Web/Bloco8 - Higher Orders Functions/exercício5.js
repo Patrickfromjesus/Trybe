8e26ecfdbe8aa4b00a9007443d144d48b77b5c76
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-24 10:39:13 -0300

    Adição exercício 5 prática pronto.

--- Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício5.js
+++ Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício5.js
@@ -1,1 +1,12 @@
 const {books} = require('./exercícios.js');
+
+//Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+const names = [];
+books.forEach((obj) => {
+  if (obj.genre.toLocaleLowerCase() === 'ficção científica' || obj.genre.toLocaleLowerCase() === 'fantasia') return names.push(obj.author.name);
+})
+names.sort();
+
+console.log(names);
+

28dd9f0630634f998e7568cbaf9cd504d8a36477
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-24 10:51:36 -0300

    Adição do exercício 6 prática pronto.

--- Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício6.js
+++ Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício6.js
@@ -1,1 +1,12 @@
 const {books} = require('./exercícios.js');
+
+// Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+const now = new Date().getFullYear();
+const oldBooks = [];
+books.forEach((obj) => {
+  if(now - obj.releaseYear > 60) oldBooks.push(obj.name); 
+})
+
+console.log(oldBooks);
+

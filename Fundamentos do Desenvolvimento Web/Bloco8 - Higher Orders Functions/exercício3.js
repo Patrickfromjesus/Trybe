a4ef9e530196abb1e70019bd3b30d45ab93801a5
Gomes Conceição <patrick_epcar17@outlook.com>
2022-06-24 10:20:31 -0300

    Adição exercício 3 prática pronto.

--- Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício3.js
+++ Fundamentos do Desenvolvimento Web/Bloco8 - Higher Orders Functions/exercício3.js
@@ -1,1 +1,7 @@
 const {books} = require('./exercícios.js');
+
+//Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+const booksGenre = books.filter((obj) => (obj.genre.toLowerCase() === 'ficção científica' || obj.genre.toLowerCase() === 'fantasia'))
+
+console.log(booksGenre);

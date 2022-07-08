1132f2f87c8213cffb5b14c501b1ff4d6ffa952d
Gomes Conceição <patrick_epcar17@outlook.com>
2022-07-07 22:44:36 -0300

    Adição do exercício bônus pronto.

--- Fundamentos do Desenvolvimento Web/Bloco9 - JS e Testes Assíncronos/exercícios/copy.js
+++ Fundamentos do Desenvolvimento Web/Bloco9 - JS e Testes Assíncronos/exercícios/copy.js
@@ -1,19 +1,14 @@
-async function transformToBRL() {
-  const url2 = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json'
-  const valueJson =  await fetch(url2);
-  const valueObj = await valueJson.json();
-  return valueObj.usd.brl;
-}
-
 async function takeCripto() {
   const url = 'https://api.coincap.io/v2/assets';
   const criptoObj = await fetch(url);
   const criptoArr = await criptoObj.json();
-  const something = await transformToBRL();
+  const url2 = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json'
+  const valueJson =  await fetch(url2);
+  const valueObj = await valueJson.json();
   criptoArr.data.forEach((element, index) => {
     if(index < 10) {
       const { name, symbol, priceUsd } = element;
-      const valueUSD = parseFloat(priceUsd) * something;
+      const valueUSD = parseFloat(priceUsd) * valueObj.usd.brl;
       createLi(`${name} (${symbol}): ${valueUSD}`);
     }
   })

9a1238cba361849a96a2b5776834bb2575c943cb
Patrick Gomes <patrick_epcar17@outlook.com>
2022-10-27 16:53:38 -0300

    update: Adição do exercício 5 pronto.

--- bmi.js
+++ bmi.js
@@ -8,4 +8,16 @@ function bmi(weight, height) {
 const weight = readline.questionFloat('What\'s your weight ');
 const height = readline.questionFloat('What\'s your height? ');
 
-console.log(bmi(weight, height));
+const bmiCalc = bmi(weight, height);
+
+console.log(bmiCalc);
+
+function stateIn(value) {
+  if (value < 18.5) return 'under weight';
+  if (value < 24.9) return 'normal weight';
+  if (value < 29.9) return 'overweight';
+  if (value < 34.9) return 'obesity I';
+  if (value < 39.9) return 'obesity II';
+  return 'obesity III and IV';
+}
+console.log(`\nYou\'re ${stateIn(bmiCalc)}`);

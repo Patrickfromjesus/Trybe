bcd221de0b17fa7fee0154e50fae387386277c0a
Patrick Gomes <patrick_epcar17@outlook.com>
2022-11-07 11:35:09 -0300

    update: Adição de morgan e o dotenv.

--- trybecash-api/src/utils/app.js
+++ trybecash-api/src/utils/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const missionsRouter = require('../routers/missionsRouters.router');
 
 const app = express();
 
+app.use(morgan());
 app.use(express.json());
 app.use('/missions', missionsRouter);
 
--- trybecash-api/src/utils/server.js
+++ trybecash-api/src/utils/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+require('dotenv').config();
 
 const PORT = 3001;
 
-app.listen(PORT, async () => console.log(`Server up on port ${PORT}.`));
+app.listen(PORT, async () =>  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`));
 
